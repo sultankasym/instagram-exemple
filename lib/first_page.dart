3131fd569d333c668bc4f03712c5a99c32bf29ab
Sultan <Sultankasymov77@gamil.com>
2024-03-27 00:13:31 +0600

    exemple insta

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -7,12 +7,12 @@ class FirstPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 230, 86, 9),
+        backgroundColor: const Color.fromARGB(255, 230, 86, 9),
         title: const Text('ListView exemple Instagram'),
       ),
       body: ListView(
         children: [
-          Container(
+          SizedBox(
             height: 150,
             // width: 300,
             // color: Color.fromARGB(255, 230, 86, 9),
@@ -37,28 +37,54 @@ class FirstPage extends StatelessWidget {
           const SizedBox(
             height: 30,
           ),
-          Container(
-            height: 200,
-            child: ListView.builder(
-              scrollDirection: Axis.vertical,
-              itemCount: 10,
-              itemBuilder: (context, count) {
-              return  Container(
-                  decoration:  const BoxDecoration(    color: Color.fromARGB(255, 230, 86, 9),),
-                  height: 50,
-                  width: 70,
-                  child: Center(
-                    child: Text(
-                      'Container $count',
-                      style: TextStyle(color: Colors.white),
+          SizedBox(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: 
+            List.generate(10, (index) {
+                  EdgeInsets.symmetric(vertical: 10, horizontal: 20);
+                 return Padding(
+                   padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                   child: Container(
+                      height: 50,
+                      color: Color.fromARGB(255, 230, 86, 9),
+                      child: Center(
+                        child: Text('Container $index', style: TextStyle(color: Colors.white)),
+                      ),
                     ),
-                  ),
-                );
-              },
+                 );
+                })
+              
             ),
-          )
+            // height: 200,
+            // child: ListView.builder(
+            //   scrollDirection: Axis.vertical,
+            //   itemCount: 10,
+            //   itemBuilder: (context, count) {
+            //   return Container(
+            //       decoration:  const BoxDecoration(    color: Color.fromARGB(255, 230, 86, 9),),
+            //       height: 50,
+            //       width: 300,
+            //       child: Center(
+            //         child: Container(
+                    
+            //           color: Colors.red,
+            //           width: double.infinity,
+            //           height: 50,
+            //           child: Text(
+            //             'Container $count',
+            //             style: const TextStyle(color: Colors.white),
+            //           ),
+            //         ),
+            //       ),
+            //     );
+            //   },
+            // ),
+          ),
+
         ],
       ),
+       
     );
   }
 }
