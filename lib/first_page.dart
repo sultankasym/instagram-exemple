4839708ece465e203fcb138d734a62dd3c598858
Sultan <Sultankasymov77@gamil.com>
2024-03-26 18:36:13 +0600

    exemple

---
+++ lib/first_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 230, 86, 9),
+        title: const Text('ListView exemple Instagram'),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 150,
+            // width: 300,
+            // color: Color.fromARGB(255, 230, 86, 9),
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, count) {
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    width: 65,
+                    height: 65,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color.fromARGB(255, 230, 86, 9),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              itemCount: 10,
+              itemBuilder: (context, count) {
+                Container(
+                  decoration: BoxDecoration(),
+                  height: 50,
+                  width: 70,
+                  color: Color.fromARGB(255, 230, 86, 9),
+                  child: Center(
+                    child: Text(
+                      'Container $count',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
