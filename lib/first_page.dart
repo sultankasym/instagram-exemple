7461c179e26e75d2025134afc70f500360a2ebe0
Sultan <Sultankasymov77@gamil.com>
2024-03-27 18:05:11 +0600

    done

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+  FirstPage({super.key});
+//   List bgimage = [
+// Image = ''
+//   ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,9 +26,11 @@ class FirstPage extends StatelessWidget {
                 return Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Container(
+
                     width: 65,
                     height: 65,
                     decoration: const BoxDecoration(
+                     
                       shape: BoxShape.circle,
                       color: Color.fromARGB(255, 230, 86, 9),
                     ),
@@ -42,14 +47,18 @@ class FirstPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: 
             List.generate(10, (index) {
-                  EdgeInsets.symmetric(vertical: 10, horizontal: 20);
+                  const EdgeInsets.symmetric(vertical: 10, horizontal: 20);
                  return Padding(
                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                    child: Container(
-                      height: 50,
-                      color: Color.fromARGB(255, 230, 86, 9),
+                    
+                      height: 300,
+                      decoration: const BoxDecoration(
+                           color: Color.fromARGB(255, 230, 86, 9),
+                         image: DecorationImage(image: NetworkImage('https://t4.ftcdn.net/jpg/04/61/47/03/360_F_461470323_6TMQSkCCs9XQoTtyer8VCsFypxwRiDGU.jpg'), fit: BoxFit.cover),),
+                   
                       child: Center(
-                        child: Text('Container $index', style: TextStyle(color: Colors.white)),
+                        child: Text('Container $index', style: const TextStyle(color: Colors.white)),
                       ),
                     ),
                  );
